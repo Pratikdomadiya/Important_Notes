ff0d8de8c1750b9abe599d4da6e7a0dd5811bd25
Pratikdomadiya <pratikdomadiya123@gmail.com>
2021-02-16 09:31:45 +0530

    m3

--- syntex.py
+++ syntex.py
@@ -4,3 +4,6 @@ for i in range(10,0,-1):# reverse loop
 t1=list_1
 t2=list_2
 temp=(set(t1).intersection(t2))# return common elements in two different list
+
+#sieve method for finding the prime number is efficient among all-->> go geeksforgeek.com
+
